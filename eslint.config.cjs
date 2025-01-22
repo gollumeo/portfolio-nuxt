e50fe55ff77a83612b20730f2f39a7c8de5c33fc
Golluméo <p.mauriello54@gmail.com>
2025-01-22 17:47:00 +0100

    chore: eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:nuxt/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2024,
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': ['error', { html: { void: 'always' } }],
+    'vue/max-attributes-per-line': ['error', { singleline: 3, multiline: 1 }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  }
+};
