983b021a678db7e94770eab339f381e59eb11247
Golluméo <p.mauriello54@gmail.com>
2025-01-23 23:39:35 +0100

    chore: i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt',
-        '@nuxtjs/i18n',
-        '@nuxt/eslint',
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxtjs/i18n',
+    '@nuxt/eslint',
+  ],
+
+  i18n: {
+    lazy: true,
+    defaultLocale: 'fr',
+    langDir: '../src/infrastructure/i18n/locales/',
+    strategy: 'prefix_except_default',
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        iso: 'en-US',
+        file: 'en.ts',
+      },
+      {
+        code: 'fr',
+        name: 'Français',
+        iso: 'fr-FR',
+        file: 'fr.ts',
+      },
     ],
+  },
 
-    pages: true,
+  pages: true,
 
-    components: {
-        dirs: ['presentation/components'],
-    },
+  components: {
+    dirs: ['presentation/components'],
+  },
 
-    imports: {
-        dirs: ['presentation/composables', '/stores'],
-    },
-    devtools: {enabled: true},
+  imports: {
+    dirs: ['presentation/composables', '/stores'],
+  },
+  devtools: { enabled: true },
 
-    css: ['/presentation/assets/main.css'],
-    dir: {
-        pages: 'presentation/pages',
-        layouts: 'presentation/layouts',
-    },
+  css: ['/presentation/assets/main.css'],
+  dir: {
+    pages: 'presentation/pages',
+    layouts: 'presentation/layouts',
+  },
 
-    srcDir: 'src/', compatibilityDate: '2024-11-01',
-})
+  srcDir: 'src/',
+
+  compatibilityDate: '2024-11-01',
+});
