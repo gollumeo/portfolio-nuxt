26cf73c11f3b80d7083c521114fa48ba97fae9b2
Golluméo <p.mauriello54@gmail.com>
2025-01-26 01:32:02 +0100

    chore(test): return type

--- tests/unit/Url.spec.ts
+++ tests/unit/Url.spec.ts
@@ -2,13 +2,13 @@ import { createUrl } from '@/domain/types/Utils/Url';
 import { urlsTestData } from '@/tests/utils/testData';
 import { describe, expect, it } from 'vitest';
 
-describe.each(urlsTestData.validUrls)('Valid URLs', (validUrl: string) => {
+describe.each(urlsTestData.validUrls)('Valid URLs', (validUrl: string): void => {
   it(`should accept ${validUrl}`, () => {
     expect(createUrl(validUrl)).toBe(validUrl);
   });
 });
 
-describe.each(urlsTestData.invalidUrls)('Invalid URLs', ({ url, expectedError }) => {
+describe.each(urlsTestData.invalidUrls)('Invalid URLs', ({ url, expectedError }): void => {
   it(`should throw error for ${url}`, () => {
     expect(() => createUrl(url)).toThrowError(expectedError);
   });
