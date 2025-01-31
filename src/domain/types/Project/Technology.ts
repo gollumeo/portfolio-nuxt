6ac93100e610a140f95544fa7a0406f6a485c800
Golluméo <p.mauriello54@gmail.com>
2025-01-31 17:39:12 +0100

    chore(Technology): add 'TailwindCSS' to type

--- src/domain/types/Project/Technology.ts
+++ src/domain/types/Project/Technology.ts
@@ -1,7 +1,7 @@
 export type Technology =
   | 'PHP' | 'TypeScript' | 'Java' | 'C#' | 'Node.js' | 'Python' // Backend Languages
   | 'Laravel' | 'Adonis' | 'Spring Boot' | '.NET' // Backend Frameworks
-  | 'CSS' | 'SASS' | 'HTML' | 'JavaScript' // Frontend Languages
+  | 'CSS' | 'TailwindCSS' | 'SASS' | 'HTML' | 'JavaScript' // Frontend Languages
   | 'Angular' | 'React' | 'Vue.js' | 'Next.js' | 'Nuxt.js' // Frontend Frameworks
   | 'MySQL' | 'MariaDB' | 'PostgreSQL' | 'SQL' | 'Prisma' | 'Eloquent' // Databases & ORMs
   | 'API' | 'REST' | 'gRPC' | 'GraphQL'; // API Types
