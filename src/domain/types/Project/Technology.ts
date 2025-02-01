dc08e15b2b5dfbccadc93e0e61bd9d3364c0d267
Golluméo <p.mauriello54@gmail.com>
2025-02-01 18:55:27 +0100

    chore(projects): fill the projects.json file & add missing tags to Technology type

--- src/domain/types/Project/Technology.ts
+++ src/domain/types/Project/Technology.ts
@@ -1,7 +1,9 @@
 export type Technology =
-  | 'PHP' | 'TypeScript' | 'Java' | 'C#' | 'Node.js' | 'Python' // Backend Languages
-  | 'Laravel' | 'Adonis' | 'Spring Boot' | '.NET' // Backend Frameworks
-  | 'CSS' | 'TailwindCSS' | 'SASS' | 'HTML' | 'JavaScript' // Frontend Languages
-  | 'Angular' | 'React' | 'Vue.js' | 'Next.js' | 'Nuxt.js' // Frontend Frameworks
-  | 'MySQL' | 'MariaDB' | 'PostgreSQL' | 'SQL' | 'Prisma' | 'Eloquent' // Databases & ORMs
-  | 'API' | 'REST' | 'gRPC' | 'GraphQL'; // API Types
+  | 'PHP' | 'TypeScript' | 'Java' | 'C#' | 'Node.js' | 'Python' // Backend languages
+  | 'Laravel' | 'Livewire' | 'Adonis' | 'Spring Boot' | '.NET' // Backend Frameworks
+  | 'CSS' | 'TailwindCSS' | 'SASS' | 'SCSS' | 'HTML' | 'JavaScript' // Frontend Languages
+  | 'Angular' | 'React' | 'Vue.js' | 'Next.js' | 'Nuxt.js' | 'Svelte' // Frontend Frameworks
+  | 'MySQL' | 'MariaDB' | 'PostgreSQL' | 'SQL' | 'Prisma' | 'Eloquent' // DB & ORMs
+  | 'API' | 'REST' | 'gRPC' | 'GraphQL' // APIs
+  | 'Machine Learning' | 'PEFT' | 'QLoRA' | 'Rope Scaling' // ML & AI
+  | 'Stripe' | 'HuggingFace' | 'Model-Controller Pattern' | 'POO'; // Concepts/misc
