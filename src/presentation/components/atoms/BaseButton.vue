6ea09820082ae3ec14bd3ca38e3f8ef314a636fa
Golluméo <p.mauriello54@gmail.com>
2025-02-05 00:37:07 +0100

    feat(baseButton): props (label) definition

---
+++ src/presentation/components/atoms/BaseButton.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div/>
+</template>
--- tests/unit/components/BaseButton.spec.ts
+++ tests/unit/components/BaseButton.spec.ts
@@ -1,7 +1,8 @@
 import BaseButton from '@/presentation/components/atoms/BaseButton';
 import type { VueWrapper } from '@vue/test-utils';
 import { mount } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+
 
 describe('BaseButton', () => {
   it('should have a "label" prop', () => {
@@ -9,6 +10,23 @@ describe('BaseButton', () => {
       props: { label: '' },
     });
 
-    expect(wrapper).toHaveProperty('label');
+    expect(wrapper.props()).toHaveProperty('label');
+  });
+
+  it('should return a default value', () => {
+    const wrapper = mount(BaseButton, {});
+
+    expect(wrapper.props().label).toBe('');
+  });
+
+  it('should fail if "label" is not a string', () => {
+    const consoleWarnSpy = vi.spyOn(console, 'warn').mockImplementation(() => {
+    });
+
+    mount(BaseButton, {
+      // @ts-expect-error - We do test, on purpose, a wrong value
+      props: { label: 123 },
+    });
+    expect(consoleWarnSpy).toHaveBeenCalled();
   });
 });
