5e54606f397cc3b65c45d6728a63b1123fde760c
Golluméo <p.mauriello54@gmail.com>
2025-02-05 21:59:26 +0100

    [ci skip] chore(config): eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,19 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 export default withNuxt({
   rules: {
     'vue/multiword-component-names': 'off',
-    'import/extensions': [
-      'error',
-      'always',
-      {
-        'js': 'never',
-        'ts': 'never',
-        'vue': 'always',
-        'json': 'never',
+  },
+  // ---- Résolution des imports ----
+  settings: {
+    'import/resolver': {
+      // 1) Le resolver TypeScript
+      typescript: {
+        // Remplace par le chemin vers ton tsconfig si besoin
+        project: './tsconfig.json',
       },
-    ],
+      // 2) Le resolver Node (utile si tu as des .vue, .json, etc.)
+      node: {
+        extensions: ['.js', '.ts', '.d.ts', '.vue', '.json'],
+      },
+    },
   },
 });
