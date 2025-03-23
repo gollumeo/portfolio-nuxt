f16f758ed93c15c263191cb3ab542e0958f48225
Golluméo <p.mauriello54@gmail.com>
2025-03-23 21:36:10 +0100

    chore: clean slate, fresh Nuxt + Nuxt UI base

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+import { defineNuxtConfig } from 'nuxt/config';
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  },
+  modules: ['@nuxt/ui'],
+  css: ['./nuxt/assets/main.css']
+})
