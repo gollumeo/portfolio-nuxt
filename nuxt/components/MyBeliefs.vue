408915854e08e2437913c24d6bc9b7efa2b37180
Golluméo <p.mauriello54@gmail.com>
2025-04-17 23:47:39 +0200

    chore(ui): add placeholder text to MyBeliefs.vue

--- nuxt/components/MyBeliefs.vue
+++ nuxt/components/MyBeliefs.vue
@@ -4,7 +4,7 @@
 
 <template>
 <h2>Mon parcours initiatique—ou mes croyances</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ex fugit praesentium sequi tenetur? Earum hic maxime modi nam necessitatibus, nisi nostrum pariatur, quaerat quis repudiandae totam veritatis. Eveniet, facilis!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum ex fugit praesentium sequi tenetur? Earum hic maxime modi nam necessitatibus, nisi nostrum pariatur, quaerat quis repudiandae totam veritatis. Eveniet, facilis! </p>
 </template>
 
 <style scoped>
